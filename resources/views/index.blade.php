13013821b0c58244d3f0e56cb15485b5f74449ad
herbetjr <herbetjr@gmail.com>
2019-05-03 02:06:55 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,7 +144,7 @@
  <div class="venda text-right ">
      <span><i class="material-icons vresiduos">close</i></span>
      <p class="text-left p-0 abrir d-none"><i class="material-icons vresiduos">arrow_back_ios</i></p>
-     <p class="text-center">Venda seu residuo ( Aqui téra uma chamada para a venda de residuos)</p>
+     <p class="text-center">Venda seu residuo ( Aqui terá uma chamada para a venda de residuos)</p>
  </div>
 
  <style>
