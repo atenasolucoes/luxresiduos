5694d4d2be5f93c7e0fa01a708da17993b25b79a
herbetjr <herbetjr@gmail.com>
2019-05-11 00:32:44 -0300

    up

--- resources/views/site/vendaseuresiduo.blade.php
+++ resources/views/site/vendaseuresiduo.blade.php
@@ -23,11 +23,76 @@
         <div class="col-sm-6">
 
         </div>
-      
+
         <div class="col-sm-12 text-center">
             <hr>
             <img src="/imagens/reciclaveis.png" width="100px;" alt="">
             <h3>Resíduos Recicláveis</h3>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/plastico.png" width="50px" alt="">
+            <h4>Plástico</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/metal.png" width="50px" alt="">
+            <h4>Metais</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/papel.png" width="50px" alt="">
+            <h4>Papel e papelão</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/vidro.png" width="50px" alt="">
+            <h4>Vidro</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/madeira.png" width="50px" alt="">
+            <h4>Madeira</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+
+        </div>
+
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/plastico.png" width="50px" alt="">
+            <h4>Borracha</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/metal.png" width="50px" alt="">
+            <h4>Sucata em geral</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/organico.png" width="50px" alt="">
+            <h4>Orgânicos</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
+        </div>
+        <div class="col-sm-3 text-center mt-1">
+            <img src="/imagens/organico.png" width="50px" alt="">
+            <h4>Sobras de produtos</h4>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos sed, repellendus voluptas consequatur eaque tempora vitae nulla voluptates, vero neque architecto et deleniti ipsam in, quia aperiam obcaecati ducimus!
+            </div>
         </div>
     </div>
 
