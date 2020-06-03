c718e5f9e05a2ff1cb0ea591f33c748caaf9bae7
herbetjr <herbetjr@gmail.com>
2020-06-03 18:45:16 -0300

    xzxzx

--- resources/views/site/vendaseuresiduo.blade.php
+++ resources/views/site/vendaseuresiduo.blade.php
@@ -23,70 +23,70 @@ $site = $conteudo->all()->first();
 <section class="container-fluid">
     <div class="row">
 
-        <div class="col-sm-6">
-            <a href="/contato"> <img src="/imagens/orcamento.png" class="w-100" alt=""></a>
+        <div class="col-sm-6 text-center p-5">
+            <a href="/contato"> <img src="/imagens/orcamento.png" style="width: 400px; max-width: 100%"></a>
         </div>
         <div class="col-sm-6">
-            <h1 class="p-5 text-justify">
+            <h2 class="p-5 text-justify">
                 @isset($site)
                 <?php echo $site->venda_texto; ?>
                 @endisset
                 <!-- Lucrar com seu resíduo nunca foi tão fácil, veja na lista abaixo quais resíduos podem ter dar lucro. -->
-            </h1>
+            </h2>
 
 
         </div>
 
 
-        <div class="col-sm-12 text-center">
+        <div class="col-sm-12 text-center mb-1">
             <hr>
-            <img src="/imagens/reciclaveis.png" width="100px;" alt="">
-            <h3>Resíduos Recicláveis</h3>
+            {{-- <img src="/imagens/reciclaveis.png" width="100px;" alt=""> --}}
+            <h1>Resíduos Recicláveis</h1>
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/plastico.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/plastico.png" width="50px" alt="">
             <h4>Plástico</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/metal.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/metal.png" width="50px" alt="">
             <h4>Metais</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/papel.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/papel.png" width="50px" alt="">
             <h4>Papel e papelão</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/vidro.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/vidro.png" width="50px" alt="">
             <h4>Vidro</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/madeira.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/madeira.png" width="50px" alt="">
             <h4>Madeira</h4>
 
 
         </div>
 
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/plastico.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/plastico.png" width="50px" alt="">
             <h4>Borracha</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/metal.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/metal.png" width="50px" alt="">
             <h4>Sucata em geral</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/organico.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/organico.png" width="50px" alt="">
             <h4>Orgânicos</h4>
 
         </div>
-        <div class="col-sm-2 text-center mt-1">
-            <img src="/imagens/organico.png" width="25px" alt="">
+        <div class="col-sm-3 text-center mt-5">
+            <img src="/imagens/organico.png" width="50px" alt="">
             <h4>Sobras de produtos</h4>
 
         </div>
